99be9bfb7b05f972b435275f113fc2499ac393d7
Ilie Burdiniuc <ilya.burdyniuk@yandex.com>
2025-01-08 19:52:55 +0200

    created a new app for predictions

--- heart_disease_web_app/settings.py
+++ heart_disease_web_app/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djongo',
-    "account.apps.AccountConfig"
+    "account.apps.AccountConfig",
+    "predictions.apps.PredictionsConfig"
 ]
 
 MIDDLEWARE = [
