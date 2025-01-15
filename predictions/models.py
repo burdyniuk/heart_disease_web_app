c23ff362e8c3d6bfc58411525ae215731b409aaa
Ilie Burdiniuc <ilya.burdyniuk@yandex.com>
2025-01-15 22:45:18 +0200

    translated to romanian

--- predictions/models.py
+++ predictions/models.py
@@ -4,37 +4,37 @@ from django.contrib.auth.models import User
 
 class Prediction(models.Model):
     SEX_CHOICES = [
-        (0, 'Female'),
-        (1, 'Male'),
+        (0, 'Femeie'),
+        (1, 'Bărbat'),
     ]
     CHEST_PAIN_CHOICES = [
-        (1, 'Typical Angina'),
-        (2, 'Atypical Angina'),
-        (3, 'Non-anginal Pain'),
-        (4, 'Asymptomatic'),
+        (1, 'Angină tipică'),
+        (2, 'Angină atipică'),
+        (3, 'Durere non-anginală'),
+        (4, 'Asimptomatic'),
     ]
     FASTING_BLOOD_SUGAR_CHOICES = [
-        (0, 'False'),
-        (1, 'True'),
+        (0, 'Sub 120 mg/dl'),
+        (1, 'Peste 120 mg/dl'),
     ]
     RESTING_ECG_CHOICES = [
         (0, 'Normal'),
-        (1, 'having ST-T wave abnormality'),
-        (2, "showing probable or definite left ventricular hypertrophy by Estes' criteria"),
+        (1, 'Anomalie undă ST-T'),
+        (2, "Prezintă hipertrofie ventriculară stângă probabilă sau certă după criteriile Estes"),
     ]
     EXERCISE_ANGINA_CHOICES = [
-        (0, 'No'),
-        (1, 'Yes'),
+        (0, 'Nu'),
+        (1, 'Da'),
     ]
     ST_SLOPE_CHOICES = [
-        (1, 'Upsloping'),
-        (2, 'Flat'),
-        (3, 'Downsloping'),
+        (1, 'Ascendent'),
+        (2, 'Plat'),
+        (3, 'Descendent'),
     ]
 
     TARGET_CHOICES = [
-        (0, 'False'),
-        (1, 'True'),
+        (0, 'Nu există risc'),
+        (1, 'Există risc'),
     ]
 
     age = models.IntegerField()
