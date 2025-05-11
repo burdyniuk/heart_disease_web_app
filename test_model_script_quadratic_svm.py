f14d0b3eadbcbb0e0d1400191169c3d701e6ae7d
Ilie Burdiniuc <ilya.burdyniuk@yandex.com>
2025-05-11 15:44:38 +0300

    updated model for prediction, outputs now and confidence

--- test_model_script_quadratic_svm.py
+++ test_model_script_quadratic_svm.py
@@ -1,13 +1,19 @@
 import quadratic_svm_prediction_model as prediction_model
+import quadratic_svm_prediction_with_probability as prediction_model_with_probability
 import matlab
 
-predictor = prediction_model.initialize()
+# predictor = prediction_model.initialize()
+pred_prob = prediction_model_with_probability.initialize()
 
 input_data = matlab.double([[44, 1, 4, 150, 412, 0, 0, 170, 0, 0.0, 1]])
+# input_data = matlab.double([[2, 57, 2, 236, 0, 0, 174, 0, 130, 2, 0]])
 
-result = predictor.quadratic_svm_prediction_model(input_data)
+# result = predictor.quadratic_svm_prediction_model(input_data)
+result, confidence = pred_prob.quadratic_svm_prediction_with_probability(input_data, nargout=2)
 
-print("Predicție:", int(result))
+print("Predicție:", result)
+print('Confidență:', confidence)
 
 # Închide componenta după utilizare
-predictor.terminate()
+# predictor.terminate()
+pred_prob.terminate()
