95bdf7335fd0a3c878cfb3a48f85d3292d8be0da
Ilie Burdiniuc <ilya.burdyniuk@yandex.com>
2025-05-30 23:15:08 +0300

    added urls

--- heart_disease_web_app/urls.py
+++ heart_disease_web_app/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('predictions/', include('predictions.urls')),
+    path('analytics/', include('analytics.urls')),
     path('', home, name='home'),
 ]
